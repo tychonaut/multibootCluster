e70379dd4ca60490cd3e7601a3abd0f77e44a646
Markus Schlüter <mschlueter@geomar.de>
2021-08-19 12:49:08 +0200

    experimenting on how to call Windows' octave from the restricted msys env (as I cannot install packages for mingws' octave)

---
+++ appControl/ParaView/misc/callWindowsOctaveFromMsys2Bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+powershell "
+C:\\Octave\\Octave-5.2.0\\mingw64\\bin\\octave-cli.exe  D:\\devel\\scripts\\multiOSCluster\\appControl\\ParaView\\misc\\convertFrustum_FovEuler_To_FOUR_PlaneCorners_forMoreViz.m"
+
+
+
+
